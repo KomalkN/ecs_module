b2037acef411c8613bbde3ca77a6b37bfb4cc07e
Komal <144211225+KomalkN@users.noreply.github.com>
2024-08-20 16:42:26 +0530

    update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,1 +1,17 @@
-nul
+# Define local variables
+locals {
+  configs = module.config.configs # Retrieve configurations from the config module
+  secrets = module.config.secret_data
+}
+
+# Invoke config module to configure common settings
+module "config" {
+  source                = "./modules/config"
+  enable_config_secrets = false
+}
+
+# Invoke Kubernetes roles module to set up EKS roles
+module "kubernetes_roles" {
+  source          = "./modules/aws_eks_role"
+  oidc_issuer_url = module.kubernetes.oidc_issuer_url
+}
